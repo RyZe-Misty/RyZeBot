252066370fcdd61d09177c88db0042c842c0aedc
RyZeStudent <77009331+RyZeStudent@users.noreply.github.com>
2021-08-26 10:42:15 +0100

    Update clear.js
    made it family friendly lol

--- commands/clear.js
+++ commands/clear.js
@@ -4,10 +4,10 @@ module.exports ={
     description: "Clear messages",
     async execute(client, message, args, Discord) {
         if(!args[0]) return message.reply("Please enter the amount of messages that you want to clear.");
-        if(isNaN(args[0])) return message.reply("Enter an actual number dipshit.");
+        if(isNaN(args[0])) return message.reply("Enter an actual number.");
 
         if (args[0] > 100) return message.reply("You can't delete more than 100 messages.");
-        if(args [0] < 1) return message.reply("You have to delete at least 1 message, I really don't know what you're trying here...");
+        if(args [0] < 1) return message.reply("You have to delete at least 1 message.");
 
         await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
             message.channel.bulkDelete(messages);
