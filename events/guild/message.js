20f802f2a447ac4196ae96401a1afce90abb9a49
RyZe-Misty <73624825+RyZe-Misty@users.noreply.github.com>
2021-09-21 08:22:32 +0100

    fixed

--- events/guild/message.js
+++ events/guild/message.js
@@ -7,8 +7,6 @@ module.exports = (Discord, client, message) =>{
 
     const command = client.commands.get(cmd);
 
-    if(command) command.execute(client, message, args, Discord);
-
     const validPermissions = [
         "CREATE_INSTANT_INVITE",
         "KICK_MEMBERS",
@@ -58,21 +56,5 @@ module.exports = (Discord, client, message) =>{
         }
     }
 
-    /* BELOW CODE IS FOR MESSAGE ANTI-SPAM PROVIDED BY the 'cuber#1001' */
-    
-    let AS = {}; //Anti-Spam
-
-    const timeAS = 5; //5 seconds
-    const msgsAS = 3; //3 messages
-
-//you will be allowed to send 3 messages in 5 seconds every message after that for the rest of the 5 seconds will be deleted.
-
-    client.on('messageCreate', async(message) => {
-        if(message.author.bot || !message.guild) return;
-        if(!AS[message.author.id]) AS[message.author.id] = {};
-        if(!AS[message.author.id][message.guild.id]) AS[message.author.id][message.guild.id] = 1, setTimeout(() => {delete AS[message.author.id][message.guild.id]}, timeAS * 1000);
-        else if(AS[message.author.id][message.guild.id] < msgsAS) AS[message.author.id][message.guild.id]++;
-        else if(AS[message.author.id][message.guild.id] >= msgsAS) await message.delete(), message.reply(`Don't spam!`).then(setTimeout(() => {e.delete()}, 5000));
-        else AS[message.author.id] = {}, AS[message.author.id][message.guild.id] = 1
-    })
+    if(command) command.execute(client, message, args, Discord);
 }
